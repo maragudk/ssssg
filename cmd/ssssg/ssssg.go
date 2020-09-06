1dd8a30f47d5287230ecc2db9296e2bfd1886860
Markus Wüstenberg <markus@wustenberg.dk>
2020-09-06 08:49:24 +0200

    Switch to TOML config file
    This gets rid of viper and its maaaany dependencies.

--- cmd/ssssg/ssssg.go
+++ cmd/ssssg/ssssg.go
@@ -5,41 +5,18 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/spf13/viper"
-
 	"ssssg"
 )
 
-const (
-	BuildDirConfigName      = "buildDir"
-	ComponentsDirConfigName = "componentsDir"
-	LayoutsDirConfigName    = "layoutsDir"
-	PagesDirConfigName      = "pagesDir"
-	StaticsDirConfigName    = "staticsDir"
-)
-
 func main() {
-	viper.SetConfigName("ssssg")
-	viper.AddConfigPath(".")
-
-	viper.SetDefault(BuildDirConfigName, "docs")
-	viper.SetDefault(ComponentsDirConfigName, "components")
-	viper.SetDefault(LayoutsDirConfigName, "layouts")
-	viper.SetDefault(PagesDirConfigName, "pages")
-	viper.SetDefault(StaticsDirConfigName, "statics")
-
-	if err := viper.ReadInConfig(); err != nil {
-		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
-			printFatal("Could not read config file.", err)
-		}
-	}
+	c := ReadConfig("./ssssg.toml")
 
 	options := ssssg.BuildOptions{
-		BuildDir:      viper.GetString(BuildDirConfigName),
-		ComponentsDir: viper.GetString(ComponentsDirConfigName),
-		LayoutsDir:    viper.GetString(LayoutsDirConfigName),
-		PagesDir:      viper.GetString(PagesDirConfigName),
-		StaticsDir:    viper.GetString(StaticsDirConfigName),
+		BuildDir:      c.BuildDir,
+		ComponentsDir: c.ComponentsDir,
+		LayoutsDir:    c.LayoutsDir,
+		PagesDir:      c.PagesDir,
+		StaticsDir:    c.StaticsDir,
 	}
 
 	var err error
@@ -50,7 +27,7 @@ func main() {
 	case "build":
 		err = ssssg.Build(options)
 	case "serve":
-		err = ssssg.Serve(viper.GetString(BuildDirConfigName))
+		err = ssssg.Serve(c.BuildDir)
 	case "version":
 		ssssg.Version()
 	case "watch":
